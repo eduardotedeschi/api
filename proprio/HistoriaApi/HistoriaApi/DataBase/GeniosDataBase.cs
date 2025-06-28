6632c23408d8f1a4c2dc00621e4a1f4f89791720
Eduardo Roberto de Castro Tedeschi <eduardoroberto9a@gmail.com>
2025-06-27 22:45:18 -0300

    API no ar

---
+++ proprio/HistoriaApi/HistoriaApi/DataBase/GeniosDataBase.cs
@@ -0,0 +1,17 @@
+﻿namespace HistoriaApi.DataBase
+{
+    public class GeniosDataBase
+    {
+
+        public string DatabaseConnection()
+        {
+            var json = File.ReadAllText("appsettings.json");
+            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json)!;
+            string connectionString = jsonObj!["ConnectionStrings"]["DatabaseConnection"];
+
+            return connectionString;
+        }
+
+        public List<genios> Show()
+    }
+}
